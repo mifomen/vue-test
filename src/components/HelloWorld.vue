42347c654d82d9fa5aad79131759d0c0b0aa7e0a
Ilya Koshelev <mifomen@yandex.ru>
2022-07-15 20:36:01 +0300

    1 chages

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+    <h1 class="hello__title">{{ msg }}</h1>
   </div>
 </template>
 
@@ -15,13 +15,10 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1 {
+.hello__title {
   margin: 40px 0 0;
-
-
   list-style-type: none;
   padding: 0;
-
   display: inline-block;
   margin: 0 10px;
   color: #42b983;
